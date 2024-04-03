01a062c8b6e9f32661c7c1e32ae26bb318b4e3ba
utkarsh <khareu550@gmail.com>
2024-04-03 20:18:15 +0530

    First Commit

---
+++ 01_basics/variables.js
@@ -0,0 +1,10 @@
+const userId = 12345
+let email = "utkarh@gmail.com"
+var city = "Kanpur"
+password = 1234
+
+email = "new@mail.com"
+city = "Jaipur"
+password = 567
+
+console.table([email, city, password])
