37f17f510f40bba7ddb69638d21b5b8147a1159f
utkarsh <khareu550@gmail.com>
2024-04-04 16:57:11 +0530

    Refactor code for better readability and update variable values

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+console.log(2+
+    4
+    ) // code readabilty should be high
+
+console.log(5)
+
+let name = "utkarsh" //string
+let age = 18 // number
+let isLoggedIn = true 
+let temperature = null;
+
+
+// number => 2 to the power 53
+// bigint
+// string => " " we defined character in string
+// boolean => true/false
+// null => standalone value
+// undefined => when value is not defined
+// symbol => unique
+
+// object
--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -1,10 +1,24 @@
 const userId = 12345
 let email = "utkarh@gmail.com"
 var city = "Kanpur"
-password = 1234
+
+let state;
+
+// password = 1234 // not allowed
+
+// => single line comment
+
+/*
+ multi line comment
+*/ 
+
+/*
+    Prefer not to use var
+    because of issue in block scope and functional scope
+*/
 
 email = "new@mail.com"
 city = "Jaipur"
 password = 567
 
-console.table([email, city, password])
+console.table([email, city, password, state])
